82f776314c1e83ac9a1089afd81bdc5989e3f48c
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-16 00:05:45 -0500

    style des buttons terminer et debut de script

---
+++ index.js
@@ -0,0 +1,4 @@
+//Declaration et initialisation des variables
+let resetBtn = document.getElementById("resetBtn");
+let startBtn = document.getElementById("startBtn");
+let stopBtn = document.getElementById("stopBtn");
