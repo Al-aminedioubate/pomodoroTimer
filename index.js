4b11bdb5423b91cb34c135cca91c10fdca52d4b0
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-16 00:06:35 -0500

    toutes les fonctions creer

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 let resetBtn = document.getElementById("resetBtn");
 let startBtn = document.getElementById("startBtn");
 let stopBtn = document.getElementById("stopBtn");
+
+//creation des fonction pour la fonctionnalite des btns
+function start() {
+	console.log("La fonction start est bonne");
+}
+function stop() {
+	console.log("La fonction stop est bonne");
+}
+function reset() {
+	console.log("La fonction reset est bonne");
+}
