2b53eab768186ef29a0003bcac1d4eda76a49e43
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-23 13:36:20 -0500

    fixed the bug and updated the code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let timerShow = document.getElementById("timerShow");
 const monSon = new Audio("alarm.mp3");
 
 //creation des fonction pour la fonctionnalite des btns
-let countdown = 1500;
+let countdown = 1500; // equivaut a 25 minutes en secondes
 let timer;
 
 function updatTimer() {
@@ -22,7 +22,7 @@ function updatTimer() {
 		.padStart(2, "0")}`;
 }
 
-function start() {
+function startTimer() {
 	timer = setInterval(function () {
 		countdown--;
 		updatTimer();
@@ -36,18 +36,18 @@ function start() {
 }
 
 //La fonction permettant d'arreté le timer
-function stop() {
+function stopTimer() {
 	clearInterval(timer);
 }
 
-//La fonction permettant de remettre le timer a valeur initiale qui est 25:00
-function reset() {
+//La fonction permettant de remettre le timer a sa valeur initiale qui est 25:00
+function resetTimer() {
 	clearInterval(timer);
 	countdown = 1500;
 	updatTimer();
 }
 
 //Creation de nos evenements
-startBtn.addEventListener("click", start);
-stopBtn.addEventListener("click", stop);
-resetBtn.addEventListener("click", reset);
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
